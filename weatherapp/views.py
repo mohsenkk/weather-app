f69c71640a598c2797f475554faf8de820950860
Mohsen Kashefikia <mohsen.kashefi77@gmail.com>
2023-03-31 16:32:20 +0330

    Add wethear information from openweather.org

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
-    return render(request, 'weatherapp/index.html')
+    appid = "ceb11f89fa585275330e6bab76e3d428"
+    URL = "https://api.openweathermap.org/data/2.5/weather"
+    PARAMS = {'q':'amsterdam', 'appid':appid, 'units':'metric'}    
+    r = requests.get(url=URL, params=PARAMS)
+    res = r.json()
+    description = res['weather'][0]['description']
+    icon = res['weather'][0]['icon']
+    temp = res['main']['temp']
+
+
+
+
+
+
+    return render(request, 'weatherapp/index.html', {'description':description, 'icon':icon, 'temp':temp})
