525090d71858365e75935353fc68440587ecb254
Mohsen Kashefikia <mohsen.kashefi77@gmail.com>
2023-04-03 12:45:44 +0100

    Add search by city form

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -3,9 +3,15 @@ import requests
 import datetime
 # Create your views here.
 def index(request):
+    
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'tehran'
+    
     appid = "ceb11f89fa585275330e6bab76e3d428"
     URL = "https://api.openweathermap.org/data/2.5/weather"
-    PARAMS = {'q':'amsterdam', 'appid':appid, 'units':'metric'}    
+    PARAMS = {'q':city, 'appid':appid, 'units':'metric'}    
     r = requests.get(url=URL, params=PARAMS)
     res = r.json()
     description = res['weather'][0]['description']
@@ -17,4 +23,4 @@ def index(request):
 
 
 
-    return render(request, 'weatherapp/index.html', {'description':description, 'icon':icon, 'temp':temp, 'day':day})
+    return render(request, 'weatherapp/index.html', {'description':description, 'icon':icon, 'temp':temp, 'day':day, 'city':city})
